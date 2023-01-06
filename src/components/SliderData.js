5f257b09a97b1f9f12108702afcd810e544b811d
jyoti987 <jyotisingh4748@gmail.com>
2023-01-06 15:00:59 +0530

    assignment done

---
+++ src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1671851021764-3cad4263e1db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDExfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1600244329730-10b702f39ddf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE0fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1671873570652-ed7a38d90fab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE1fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1565503187147-6b0012bab4da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+    },
+];
